e8b923fbd181b5e12ce9dc4861cc8b075999085c
luoshuibing <luoshuibing@126.com>
2021-09-21 19:35:10 +0800

    hbase修改项目结构

--- hbase-debug/src/main/java/com/jd/hbase/TestHbaseApi5.java
+++ hbase-debug/src/main/java/com/jd/hbase/TestHbaseApi5.java
@@ -33,8 +33,6 @@ public class TestHbaseApi5 {
         // bs[1]= Bytes.toBytes("1|");
         // admin.createTable(td,bs);
         // System.out.println("表格创建成功");
-
-
         Table empTable = connection.getTable(TableName.valueOf("emp1"));
         String rowkey="zhangsan";
         rowkey = HBaseUtil.genRegionNum(rowkey, 3);
